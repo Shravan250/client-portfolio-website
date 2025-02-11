310b3919a82cc3109457ac2eb59cc4a98b2b32d8
Shravan250 <shravanbobade25@gmail.com>
2025-02-11 22:52:35 +0530

    solved error

--- client/config/config.js
+++ client/config/config.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5002";
+const API_URL = "https://client-portfolio-website-backend.onrender.com";
 //http://localhost:5002
 // https://client-portfolio-website-backend.onrender.com
 export default API_URL;
